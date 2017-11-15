9a17561a7795e14f246efff889ad6cd67887fb76
User 01 <user01@unst17.(none)>
2017-11-15 16:57:09 +0000

    added ex. 10

---
+++ python_work_RW/10-sets-dictionaries.py
@@ -0,0 +1,36 @@
+print
+print "Scripts and Libraries Exercise"
+print
+
+# part 1 - sets
+
+a = set([0, 1, 2, 3, 4, 5]) # creates a set, a
+b = set([2, 4, 6, 8]) # creates a set, b
+
+print a.union(b) # conjoins the two, but 2 and 4 don't appear twice as its a set
+
+print a.intersection(b) # prints the numbers where the two sets are the same
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print 
