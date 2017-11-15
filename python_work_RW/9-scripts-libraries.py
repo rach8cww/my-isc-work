1b4b1ad739a66a553758c24040e28b31f0f8ae08
User 01 <user01@unst17.(none)>
2017-11-15 16:38:53 +0000

    ex. 9 added

---
+++ python_work_RW/9-scripts-libraries.py
@@ -0,0 +1,48 @@
+print
+print "Scripts and Libraries Exercise"
+print
+
+# part 1
+
+import dancing.dance
+from dancing. dance import boogie
+import sys
+
+"""
+made a new directory (mkdir) inside python_work_RW called "dancing" and within
+that made a directory called __init__.py.
+Then can import dancing
+"""
+
+# part 2 - checking what the boogie function does (aka reading the code in file)
+"""
+with open ('dance.py', 'r') as shoes:
+	data = shoes.read()
+	print data
+"""
+
+# part 3
+
+moves = sys.argv[1:]
+# make some new dance moves:
+dancing_queen = ["twirl", "jump", "sway", "cartwheel", "backflip"]
+boogie(dancing_queen) # calls out the function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print
